2d5022902a3cb8fd2c7bf84a9e3e0d3146c4b57f
Okay <motoalaba@gmail.com>
2018-06-12 15:07:52 +0200

    Add ActivitzChooser,ScopeDefiner

--- src/components/ContentPage.tsx
+++ src/components/ContentPage.tsx
@@ -3,22 +3,42 @@ import { PageStates } from './main';
 
 type TProps = {
   page: PageStates;
-  onNextClicked: () => void;
+  onNextClicked: (selectedLanguage: string, activityChooser: string, scopeDefiner: string) => void;
 };
+
+// bekommt page und onNextClicked vom Type TProps
 export default ({page, onNextClicked}: TProps) => {
   const content = page === PageStates.WelcomePage
-    ? renderWelcome()
+    ? renderWelcome(onNextClicked)
     : page === PageStates.DescriptionPage
       ? renderDescription()
       : renderPostGame();
   return (
     <div className="page">
       {content}
-      <button onClick={onNextClicked}>Next</button>
     </div>
   );
 };
-const renderWelcome = () => {
+
+export const AvailableLanguages = [
+    'german',
+    'english',
+    'czech',
+    'finnish'
+];
+/*
+export const ActivityChooser = [
+  'doctor',
+  'programmer',
+  'Mcdonald worker',
+  'test'
+];
+ */
+
+
+const renderWelcome = (onLanguageClicked: (language: string, activityChooser: string, scopeDefiner: string ) => void) => {
+  let Activity : HTMLSelectElement;
+  let Scope: HTMLSelectElement;
   return (
     <div>
       <h1>Welcome!</h1>
@@ -27,18 +47,43 @@ const renderWelcome = () => {
         Leberkas chuck turkey swine prosciutto. Pork chop pancetta tri-tip, short loin filet mignon venison burgdoggen.
         Pork chop frankfurter swine sirloin jowl bacon. Bacon ribeye sirloin, chuck picanha biltong meatball pastrami
         corned beef ham short loin ham hock.</p>
+        <h4>Choose a Activity</h4>
+
+        <select ref={(element) => Activity = element}>ActivityChooser
+            <option value="doctor">Doctor</option>
+            <option value="programmer">Programmer</option>
+            <option value="Mcdonald worker">Mcdonald worker</option>
+            <option value="test">Test</option>
+        </select>
+
+        <h4>What is your Scope</h4>
+
+        <select  ref={(element) => Scope = element}>ScopeDefiner
+            <option value="Car">Car</option>
+            <option value="Big Mac">Big Mac</option>
+            <option value="Test1">test1</option>
+            <option value="test2">Test2</option>
+        </select>
+
+        <h4>Choose a language!</h4>
+        {AvailableLanguages.map((language, index) => {
+            return (
+                <button key={index} onClick={() => onLanguageClicked(language, Activity.value, Scope.value)}>{language}
+                </button>
+            );
+
+        })}
+
     </div>
   );
 };
+
 const renderDescription = () => {
   return (
     <div>
       <h1>Description!</h1>
       <p>
-        Bacon ipsum dolor amet corned beef meatball boudin frankfurter landjaeger.
-        Leberkas chuck turkey swine prosciutto. Pork chop pancetta tri-tip, short loin filet mignon venison burgdoggen.
-        Pork chop frankfurter swine sirloin jowl bacon. Bacon ribeye sirloin, chuck picanha biltong meatball pastrami
-        corned beef ham short loin ham hock.</p>
+        test test test test3 test3</p>
     </div>
   );
 };
@@ -47,10 +92,7 @@ const renderPostGame = () => {
     <div>
       <h1>PostGame!</h1>
       <p>
-        Bacon ipsum dolor amet corned beef meatball boudin frankfurter landjaeger.
-        Leberkas chuck turkey swine prosciutto. Pork chop pancetta tri-tip, short loin filet mignon venison burgdoggen.
-        Pork chop frankfurter swine sirloin jowl bacon. Bacon ribeye sirloin, chuck picanha biltong meatball pastrami
-        corned beef ham short loin ham hock.</p>
+        test test test test test4.</p>
     </div>
   );
 };
