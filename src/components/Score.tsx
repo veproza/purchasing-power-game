e10ee5bcc361731961f8d7bc53531657d5fcca73
Milan <DcBexter@gmx.net>
2018-06-12 15:20:10 +0200

    added score progress boxes

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -13,17 +13,34 @@ export default class Score extends Component<{ currentScore: number, maximumScor
     }
 
     @bind
-    addScore() {
-        let nextScore = this.state.currentScore + 1;
-        this.setState({currentScore: nextScore});
+    addScore(scorePoints: number) {
+        this.setState({currentScore: this.state.currentScore + scorePoints});
+    }
+
+    addScoreBoxes() {
+        let scoreBoxes = [];
+        if (this.state.currentScore >= this.props.maximumScore) {
+            scoreBoxes.push(<span className="score-winner">YOU WIN!</span>);
+            return scoreBoxes;
+        }
+        for (let i = 0; i < this.props.maximumScore; i++) {
+            if (i + 1 <= this.state.currentScore) {
+                scoreBoxes.push(<span className="score-box scored"/>);
+            } else {
+                scoreBoxes.push(<span className="score-box"/>);
+            }
+        }
+        return scoreBoxes;
     }
 
     render() {
         return (
-            <div>Your progress is: {this.state.currentScore}/{this.props.maximumScore}
-                <div>
-                    <button onClick={this.addScore}>Add Score</button>
+            <div>
+                <span>Your progress:</span>
+                <div class="score-wrapper">
+                    {this.addScoreBoxes()}
                 </div>
+                <button onClick={this.addScore.bind(this, 2)}>Add 2 Scorepoints (debug)</button>
             </div>);
     }
 }
